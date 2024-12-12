0ce9482c8731a11eb4f76fed1b2a8dba9b8863b2
Miro <miro.grare@student.ap.be>
2024-12-12 15:15:01 +0100

    implement stap 2 en 3

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+
+class Afspraak(BaseModel):
+    titel: str
+    dag: int
+    maand: str
+    jaar: int
+
+
+afspraken = [
+    {"titel": "dokter afspraak", "dag": 12, "maand": "mei", "jaar": 2025},
+    {"titel": "tandarts afspraak", "dag": 15, "maand": "mei", "jaar": 2025},
+    {"titel": "kapper afspraak", "dag": 18, "maand": "mei", "jaar": 2025},
+]
 app = FastAPI()
+
+
+@app.post("/")
+async def create_afspraak(afspraak: Afspraak):
+    afspraken.append(afspraak)
+    return afspraak
+
+
+@app.delete("/{titel}")
+async def delete_afspraak(titel: str):
+    for i in range(len(afspraken)):
+        if afspraken[i]["titel"] == titel:
+            afspraken.pop(i)
+            return {"message": "Afspraak verwijderd"}
 
 
 @app.get("/")
 async def root():
-    return {
-        "afspraken": {
-            "afspraak1": {
-                "titel": "Docter Appointment",
-                "dag": 12,
-                "maand": "januari",
-                "jaar": 2025,
-            },
-            "afspraak2": {
-                "titel": "cinema",
-                "dag": 30,
-                "maand": "januari",
-                "jaar": 2025,
-            },
-            "afspraak3": {
-                "titel": "verjaardag",
-                "dag": 1,
-                "maand": "februari",
-                "jaar": 2025,
-            },
-        }
-    }
+    return afspraken
